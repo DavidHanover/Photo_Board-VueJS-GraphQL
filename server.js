2b4866c2f9b23949201cbb05d52907aca096e8a6
DavidHanover <DavidHanover89@gmail.com>
2019-02-19 18:36:54 -0800

    added some comments

--- server.js
+++ server.js
@@ -3,19 +3,23 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 
+//import typedefs and resolvers
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
 const resolvers = require("./resolvers");
 
+//import environment variables and mongoose models
 require("dotenv").config({ path: "variables.env" });
 const User = require("./models/User");
 const Post = require("./models/Post");
 
+//connect to mlab database
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("DB Connected"))
   .catch(err => console.error(err));
 
+//create apollo/graphql server using typedefs, resolvers, and context object
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -25,6 +29,7 @@ const server = new ApolloServer({
   }
 });
 
+//listening on default port 4000
 server.listen().then(({ url }) => {
   console.log(`Server listening on ${url}`);
 });
